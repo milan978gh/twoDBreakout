c6a9bf4b7697c2439abf325c0b86fab758fa8e7e
milan978gh <amilandev78@gmail.com>
2021-02-14 18:19:58 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,86 @@
+const grid = document.querySelector('.grid');
+const boardWidth = 560;
+const boardHeight = 300;
+const blockWidth = 100;
+const blockHeight = 20;
+const ballDiameter = 20;
+const userStart = [230, 10];
+let currentPosition = userStart;
+const ballStart = [270, 40];
+let ballCurrentPosition = ballStart;
+let timerId;
+let score = 0;
+
+
+document.addEventListener('keydown', moveUser);
+
+
+// create Block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+// all blocks
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210)
+];
+// draw all blocks
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.style.left = blocks[i].bottomLeft[0] + 'px';
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+    grid.appendChild(block);
+  }
+}
+addBlocks();
+
+
+// add user
+const user = document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
+// draw user
+function drawUser() {
+  user.style.left = currentPosition[0] + 'px';
+  user.style.bottom = currentPosition[1] + 'px';
+}
+// move user
+function moveUser(e) {
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        drawUser();
+      }
+      break;
+    case 'ArrowRight':
+      if (currentPosition[0] < (boardWidth - blockWidth)) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
+      break;
+  }
+}
+
+
